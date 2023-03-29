111284967b7ecdb79d06497f109ad7e323757cc3
Fabian Torres <programacioncursos2022@gmail.com>
2023-03-29 12:30:43 -0500

    New Feature: Add new Feactures

--- Truco.py
+++ Truco.py
@@ -1,9 +1,36 @@
 from Barajas import Baraja_Inglesa
-from Truco_funciones import barajar
+from Truco_funciones import *
 import random
 
 def run():
-    
-    barajar(Baraja_Inglesa)
-
+    saltos(2)
+    mensaje("¡Buenas!¡Soy Akinator!\U0001F9DE\n¡Encantado de conocerte!\U0001F44B\n¿Sabias que puedo leer tu mente? Aunque no lo creas, soy capaz de penetrar tus neuronas y saber lo que piensas,\nte lo demostraré con el siguiente juego:\n")
+    saltos(1)
+    mensaje("Descripción del juego:\n")
+    mensaje("El juego se llama las 21 cartas, consiste en que yo seleccionaré aleatoriamente 21 cartas\nde una de las barajas que estan disponibles,luego tendrás que pensar en una carta y\ndecirme la columna dónde se encuentra dicha carta, este proceso se repetirá 3 veces y al final revelaré la carta que pensaste.")
+    barajar_baraja = barajar(Baraja_Inglesa)
+    mazo_seleccionado = random.sample(barajar_baraja, k=21)
+    print(mazo_seleccionado)
+    print(mostrarmazo(mazo_seleccionado))
+    mazo_actual = mazo_seleccionado
+    vuelta = 1
+    while vuelta <= 3:
+        P = int(input("¿En cuál columna se encuentra tu carta? (1,2,3): "))
+        if P == 1:
+            mazo_actual = recogermazo(mazo_actual, columna_superior= 1 , columna_media = P-1, columna_inferior = 2)
+            print(mostrarmazo(mazo_actual))
+            vuelta += 1
+        elif P == 2:
+            mazo_actual = recogermazo(mazo_actual, columna_superior= 0 , columna_media = P-1, columna_inferior = 2)
+            print(mostrarmazo(mazo_actual))
+            vuelta += 1
+        elif P == 3:
+            mazo_actual = recogermazo(mazo_actual, columna_superior= 0 , columna_media = P-1, columna_inferior = 1)
+            print(mostrarmazo(mazo_actual))
+            vuelta += 1
+        else:
+            print("Columna no reconocida.")
+            continue
+    print("Tu carta es: ", adivinar(mazo_actual))
+run()
 
--- Truco_funciones.py
+++ Truco_funciones.py
@@ -5,6 +5,12 @@ from Barajas import *
 def clear():
     os.system("clear")
 
+def saltos(num):
+    for i in range(num-1):
+        print()
+def mensaje(mensaje):
+    print(mensaje)
+
 def barajar(baraja):
     auxiliar = []
     for i in range(len(baraja)):
@@ -15,29 +21,29 @@ def barajar(baraja):
     baraja = auxiliar
     return baraja
 
-def mostrarbaraja(baraja):
+def mostrarmazo(baraja):
+
+    indice = 0
+    for i in range(0,7):
+        for j in range(0,3):
+            print(baraja[indice], end="  ")
+            indice += 1
+        print("")
+
+def recogermazo(baraja, columna_superior, columna_media, columna_inferior):
     
-    for fila in range(5):
-        for columna in range(10):
-            for carta in baraja[fila][columna]:
-                ancho = 3
-                largo = 2
+    index_list = [columna_superior, columna_media, columna_inferior]
+    mazo_recogido = []
+    for i in index_list:
+        incremento = i
+        for j in baraja:
+            try:
+                mazo_recogido.append(baraja[incremento])
+                incremento += 3
+            except:
+                pass         
+    print(mazo_recogido)
+    return mazo_recogido
 
-                esp = " "
-                for i in range(0, ancho):
-                    esp = esp + "-"
-                for j in range(0, largo):
-                    esp = esp + "\n|"
-                    for k in range(0, ancho):
-                        if k == 1:
-                            esp = esp + carta
-                            continue
-                        else:
-                            esp = esp + " "
-                    esp = esp + "|"
-                esp = esp + "\n "
-                for u in range(0,ancho):
-                    esp = esp + "-"
-                print(esp)
-
-mostrarbaraja(Baraja_Inglesa)
+def adivinar(baraja):
+    return baraja[10]
